30028bc3893d523c5c7b8267966cb79651df9ee5
jackerobles <jacke.robles12@gmail.com>
2024-12-14 09:41:08 -0400

    signup finish

--- registro/urls.py
+++ registro/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
+from registro.views import SignUpView
 
 urlpatterns = [
-    path("accounts/",include('django.contrib.auth.urls'))
-    path("signup/",SignupView.as_view(),name="signup")
-]
+    path("accounts/",include('django.contrib.auth.urls')),
+    path("",SignUpView.as_view(),name="signup")
+    ]
--- registro/views.py
+++ registro/views.py
@@ -3,7 +3,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
-class SignUpView(CreateView)
+class SignUpView(CreateView):
     
     form_class=UserCreationForm
     success_url=reverse_lazy("login")
